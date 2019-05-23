1bbeb9fda7fca053e0b867e802417d61042196a4
CoffeBee <ivan.podvorniy@yandex.ru>
2019-05-23 08:33:29 +0000

    fix bugs

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import sched
 import time
 token ='650690422:AAEsDalv8DDRAqZYW-bz_3LhcCrc9NqYujI'
 bot = telebot.TeleBot(token)
-answers = []
 Counter = {}
 s = sched.scheduler(time.time, time.sleep)
 
@@ -17,14 +16,20 @@ If you want to stop working with me send "stop"''')
    bot.send_message(message.chat.id,'Answer to me, do you like this picture. Answer only "/yes" or "/no"')
    
 def send_photo(message):
+   print(1)
    bot.send_photo(message.chat.id, open('Vasnetsov/{}.jpg'.format(2), 'rb'))
    bot.send_message(message.chat.id,'Answer to me, do you like this picture. Answer only "/yes" or "/no"')
 
 @bot.message_handler(commands=['yes'])
-def answer_function_good(message,counter):
-   Counter[chat_id] = Counter(get(chat_id, val)) + 1
-   if Counter[chat_id] == 10:
-      bot.send.message(chat_id, 'If you want to see the pictures of great artistse in any time send me "/anytime"')
+def answer_function_good(message):
+   if (message.chat.id in Counter.keys()):
+      Counter[message.chat.id] = Counter[message.chat.id] + 1
+   else:
+      Counter[message.chat.id] = 1
+   if Counter[message.chat.id] == 1:
+      bot.send_message(message.chat.id, 'If you want to see the pictures of great artistse in any time send me "/anytime"')
+   elif (Counter[message.chat.id] > 1):
+      s.run()
    else:
       send_photo(message)
 
@@ -33,8 +38,8 @@ def answer_function_bad(message):
    pass
 
 @bot.message_handler(commands=['anytime'])
-def aytime(message, scheduler):
-   bot.send.message(message.chat.id,'''If you want to see pictures every hour send me "/hour",
+def aytime(message):
+   bot.send_message(message.chat.id,'''If you want to see pictures every hour send me "/hour",
 If you want to see pictures every half of day send me "/half_day"
 If you want to see pictures every day send me "/day"''')
    
@@ -44,15 +49,15 @@ def stop (message):
    del Counter[message.chat.id]
 
 @bot.message_handler(commands=['hour'])
-def every_hour_interval(messge):
-   s.enter(3600, 1, send_photo,argument = (message.chat.id))
-
+def every_hour_interval(message):
+   s.enter(15, 1, send_photo,argument = (message,))
+   s.run()
 @bot.message_handler(commands=['half_day'])
-def every_half_of_day_interval(messge):
-   s.enter(43200, 1, send_photo,argument = message)
-
+def every_half_of_day_interval(message):
+   s.enter(43200, 1, send_photo,argument = (message,))
+   s.run()
 @bot.message_handler(commands=['day'])
-def every_day_interval(messge):
-   s.enter(86400, 1, sendp_hoto,argument = message)
-     
+def every_day_interval(message):
+   s.enter(86400, 1, sendp_hoto,argument = (message,))
+   s.run()  
 bot.polling(none_stop = True)
