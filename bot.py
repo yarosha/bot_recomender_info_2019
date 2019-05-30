973b2bb701688a38ab94f57e24ae94b1a0d2ab63
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2019-05-30 08:44:43 +0300

    change language

--- bot.py
+++ bot.py
@@ -12,8 +12,7 @@ s = sched.scheduler(time.time, time.sleep)
 def gen_markup():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
-    markup.add(InlineKeyboardButton("Yes", callback_data=f"cb_yes"),
-                               InlineKeyboardButton("No", callback_data=f"cb_no"))
+    markup.add(InlineKeyboardButton("Yes", callback_data=f"cb_yes"), InlineKeyboardButton("No", callback_data=f"cb_no"))
     return markup
 
 
